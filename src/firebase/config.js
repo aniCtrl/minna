611a7d695f92b87e5d3521094b259f952131f1c0
Bahnishikha Singha <aniopt06@gmail.com>
2026-08-12 00:57:59 +0530

    feat: add Firebase authentication setup in config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,6 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -23,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
